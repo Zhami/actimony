7dda908f9916a4192f030181d285afcff186c5e0
Stuart Malin <stuart@akamai.com>
2014-11-26 18:33:42 -0500

    small progress implementing Web Sockets transport layer

--- example.js
+++ example.js
@@ -8,11 +8,18 @@
 
 "use strict";
 
-var	Actimony = require('./Lib/Actimony');
+var	Actimony	= require('./Lib/Actimony.js');
+var	Node		= require('./Lib/Node.js');
+var TransportWS	= require('./lib/TransportWS.js');
 
-var actimony = new Actimony();
+var actimony	= new Actimony();
 
-var node = actimony.getNode();
+var node1		= new Node();
 
-console.log('Node:nodeID:', node.getNodeID());
+node1.addServer(TransportWS, {port: 8080});
 
+console.log('node1: nodeID:', node1.getNodeID());
+
+
+var node2		= new Node();
+node2.connectWith(TransportWS, 'ws://localhost:8080');
