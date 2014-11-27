d18b0ae306dfbb94bc07f30ed893d7b196afa3b0
Stuart Malin <stuart@akamai.com>
2014-11-27 10:09:13 -0500

    Refined Transport to distinguish being a 'server' or a 'client'.

--- example.js
+++ example.js
@@ -15,11 +15,11 @@ var TransportWS	= require('./lib/TransportWS.js');
 var actimony	= new Actimony();
 
 var node1		= new Node();
-
-node1.addServer(TransportWS, {port: 8080});
-
 console.log('node1: nodeID:', node1.getNodeID());
+node1.addServer(TransportWS, {port: 8080});
 
 
 var node2		= new Node();
+console.log('node2: nodeID:', node2.getNodeID());
 node2.connectWith(TransportWS, 'ws://localhost:8080');
+
